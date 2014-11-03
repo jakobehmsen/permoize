994ef776b4334ae9fd4be6af4347a33141c9dc97
Jakob Brandsborg Ehmsen <jakobehmsen.github@gmail.com>
2014-11-03 16:44:04 +0100

    Added simple usage example.
    Signed-off-by: Jakob Brandsborg Ehmsen <jakobehmsen.github@gmail.com>

---
+++ eclipse/src/permoize/misc/Main.java
@@ -0,0 +1,30 @@
+package permoize.misc;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+import permoize.CommonMemoizer;
+import permoize.CommonMemoizeContainer;
+import permoize.Memoizer;
+import permoize.StreamMemoizeEntryList;
+
+public class Main {
+	public static void main(String[] args) {
+		try {
+			Memoizer memoizer = new CommonMemoizer(new CommonMemoizeContainer(new StreamMemoizeEntryList("me.mor")));
+			
+			String name = memoizer.recollect("Hello", () -> {
+				System.out.println("Hi there!! Welcome to your first visit to this application!!!");
+				System.out.println("Please enter your name, such that I can remember you in the future:");
+				
+				BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+				
+				return br.readLine();
+			});
+			
+			System.out.println("Hello " + name);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}

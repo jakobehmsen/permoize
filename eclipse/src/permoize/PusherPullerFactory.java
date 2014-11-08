9b7f0a1166f074a165cf296a8e1f765b2cd8d926
Jakob Brandsborg Ehmsen <jakobehmsen.github@gmail.com>
2014-11-08 11:42:43 +0100

    Added comment about possible renaming.
    Signed-off-by: Jakob Brandsborg Ehmsen <jakobehmsen.github@gmail.com>

--- eclipse/src/permoize/PusherPullerFactory.java
+++ eclipse/src/permoize/PusherPullerFactory.java
@@ -1,5 +1,6 @@
 package permoize;
 
+// Consider: Should be named MetaProtocol instead?
 public interface PusherPullerFactory<T, P> {
 	Puller<T> createPuller(Memoizer memoizer);
 	P createPusher(Puller<T> puller);

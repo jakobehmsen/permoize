4945eeb49be77335daeba86573a33f14546130a0
Jakob Brandsborg Ehmsen <jakobehmsen.github@gmail.com>
2014-11-17 19:38:06 +0100

    Created class SerializingRequest to represent requests that can be used both for plays, where references are kept intact, and replays where serialization is used.
    Signed-off-by: Jakob Brandsborg Ehmsen <jakobehmsen.github@gmail.com>

---
+++ eclipse/src/permoize/SerializingRequest.java
@@ -0,0 +1,14 @@
+package permoize;
+
+import java.io.Serializable;
+
+public class SerializingRequest implements Serializable {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	/*
+	Instances should somehow represent both a play context and a replay context.
+	*/
+}
